7b188f32925c5600fa7339c3a1a4009ced7e1233
Idirect-Tech <115805427+Idirect-Tech@users.noreply.github.com>
2023-03-30 15:51:01 +0530

    Update start_using.py

--- start_using.py
+++ start_using.py
@@ -7,17 +7,39 @@ api_session = 'INSERT_YOUR_API_SESSION_HERE'
 # Select Stock (USE SYMBOL AS SHOWN ON NSE) eg: RELIANCE
 STOCK = 'RELIANCE' 
 
-# Import Libraries
-from pip._internal import main as pipmain
-pipmain(['install', 'breeze_connect'])
-from breeze_connect import BreezeConnect
+import sys, subprocess, pkg_resources
+module=True
 
-# Setup my API keys 
-api = BreezeConnect(api_key=api_key)
-api.generate_session(api_secret=api_secret,session_token=api_session)
+def install():
+    python = sys.executable
+    subprocess.check_call([python, '-m', 'pip', 'install', 'breeze_connect'], stdout=subprocess.DEVNULL)
+    print('Successfully installed Breeze !!')
 
-STOCK = api.get_names('NSE', STOCK)['isec_stock_code']
+while(module):
+    try:
+        from breeze_connect import BreezeConnect
+        module=False
+    except ModuleNotFoundError:
+        print("module 'breeze_connect' is not installed. Installing it now...")    
+        install()
 
-api.place_order(stock_code=STOCK,
-                exchange_code="NSE",product="cash",action="buy",
-                order_type="market",stoploss="",quantity="1",price="",validity="day")
+try:
+    # Connecting to demat account 
+    api = BreezeConnect(api_key=api_key)
+    api.generate_session(api_secret=api_secret,session_token=api_session)
+    print("Successfully connected to demat account !!")    
+    
+except NameError:
+    print("Connection to demat account failed !!")
+    
+try:
+    STOCK = api.get_names('NSE', STOCK)['isec_stock_code']
+    order = api.place_order(stock_code=STOCK,exchange_code="NSE",product="cash",action="buy",order_type="market",stoploss="",quantity="1",price="",validity="day")
+    
+    if(order['Status'] == 200):
+        print(order['Success']['message'])
+    else:
+        print(order['Error'])
+            
+except Exception as e:
+    print("API request failed. Please try again !!/n", e)
