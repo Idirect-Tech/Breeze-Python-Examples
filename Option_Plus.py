7c16ca5a50043cd6088be3a73ac7eb67073ab455
Idirect-Tech <115805427+Idirect-Tech@users.noreply.github.com>
2023-04-12 13:09:50 +0530

    Update Option_Plus.py

--- Option_Plus.py
+++ Option_Plus.py
@@ -43,7 +43,9 @@ option_plus_order = api.place_order(stock_code="NIFTY",
 print(option_plus_order)
 
 fresh_order_id = option_plus_order["Success"]["order_id"]
-cover_order_id = str(int(fresh_order_id)+1)
+detail = api.get_order_detail('NFO',fresh_order_id)
+cover_order_id = detail['Success'][0]['parent_order_id']
+
 
 # To square off option plus order :
 # In Option Plus order, to square off your position - simply convert cover order from limit to market. 
