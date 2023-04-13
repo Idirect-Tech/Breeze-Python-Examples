221b829d6d07ac5523b9cd673206e8f996fbd9b6
Idirect-Tech <115805427+Idirect-Tech@users.noreply.github.com>
2023-04-13 12:27:30 +0530

    Update Option_Plus.py

--- Option_Plus.py
+++ Option_Plus.py
@@ -22,25 +22,30 @@ api.generate_session(api_secret=api_secret,
 # Below code will place 2 orders - fresh order and cover SLTP order.
 # Cover SLTP order is the limit order which squares off your fresh order at limit price when the price hits stop loss trigger price.
 
+# Place Option Plus order. 
+# Below code will place 2 orders - fresh order and cover SLTP order.
+# Cover SLTP order is the limit order which squares off your fresh order at limit price when the price hits stop loss trigger price.
+
 option_plus_order = api.place_order(stock_code="NIFTY",
                                    exchange_code="NFO",
                                    product="optionplus",
                                    action="buy",
-                                   order_type="limit",
+                                   order_type="Limit",
                                    quantity="50",
-                                   price="0.05", #limit price
-                                   stoploss='1', # SLTP
+                                   price="4", #limit price
+                                   stoploss='10', # SLTP
                                    validity="day",
                                    disclosed_quantity="0",
-                                   validity_date="2023-03-20T06:00:00.000Z",
-                                   expiry_date="2023-03-23T06:00:00.000Z",
-                                   right="call",
-                                   strike_price="17500",
-                                   order_type_fresh="limit",
-                                   order_rate_fresh="2", # fresh order price. 
+                                   validity_date="2023-04-13T06:00:00.000Z",
+                                   expiry_date="2023-04-13T06:00:00.000Z",
+                                   right="put",
+                                   strike_price="17750",
+                                   order_type_fresh="Market", #Limit or Market
+                                   order_rate_fresh="15", # fresh order price. 
                                    user_remark="Test")
 
 print(option_plus_order)
+
 
 fresh_order_id = option_plus_order["Success"]["order_id"]
 detail = api.get_order_detail('NFO',fresh_order_id)
@@ -53,12 +58,12 @@ cover_order_id = detail['Success'][0]['parent_order_id']
 modify_order = api.modify_order(order_id=cover_order_id,
                     exchange_code="NFO",
                     order_type="market",
-                    stoploss="",
+                    stoploss="0",
                     quantity="50",
-                    price="",
-                    validity="day",
+                    price="0",
+                    validity="Day",
                     disclosed_quantity="0",
-                    validity_date="2023-03-20T06:00:00.000Z")
+                    validity_date="2023-04-13T06:00:00.000Z")
 
 
 print(modify_order)
