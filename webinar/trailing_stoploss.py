e47878a86aed791415c4f9921f848af31f5b93e8
Idirect-Tech <115805427+Idirect-Tech@users.noreply.github.com>
2023-04-20 22:22:43 +0530

    Update trailing_stoploss.py

--- webinar/trailing_stoploss.py
+++ webinar/trailing_stoploss.py
@@ -4,8 +4,19 @@ api_key = "INSERT_YOUR _APP_KEY_HERE"
 api_secret = "INSERT_YOUR_SECRET_KEY_HERE"
 api_session = 'INSERT_YOUR_API_SESSION_HERE'
 
-# Import Libraries
 
+
+# Define Contract
+stock = 'NIFTY',
+strike = '17750',
+expiry = '2023-04-20T06:00:00.000Z',      
+right = 'call',         
+
+
+
+
+
+# Import Libraries
 from datetime import datetime
 from breeze_connect import BreezeConnect
 
@@ -13,76 +24,68 @@ from breeze_connect import BreezeConnect
 api = BreezeConnect(api_key=api_key)
 api.generate_session(api_secret=api_secret,session_token=api_session)
 
-if __name__ == "__main__":
-    print ("Starting Execution \n")
-        
-    # Define Contract
-    stock = 'NIFTY',
-    strike = '17750',
-    expiry = '2023-04-20T06:00:00.000Z',      
-    right = 'call',         
+# Place order
+buy_order = api.place_order(stock_code=stock,
+                            exchange_code="NFO",
+                            product="options",
+                            action='buy',
+                            order_type='market',
+                            stoploss="",
+                            quantity="50",
+                            price="",
+                            validity="day",
+                            validity_date=today,
+                            disclosed_quantity="0",
+                            expiry_date=expiry,
+                            right=right,
+                            strike_price=strike)
 
-    
-    # Place order
-    buy_order = api.place_order(stock_code=stock,
-                                exchange_code="NFO",
-                                product="options",
-                                action='buy',
-                                order_type='market',
-                                stoploss="",
-                                quantity="50",
-                                price="",
-                                validity="day",
-                                validity_date=today,
-                                disclosed_quantity="0",
-                                expiry_date=expiry,
-                                right=right,
-                                strike_price=strike)
+# Store Order ID
+order_id = buy_order['Success']['order_id']
 
-     
-    order_id = buy_order['Success']['order_id']
-        
-    price = api.get_quotes(stock_code=stock,
-                    exchange_code="NFO",
-                    product_type="options",
+# Get current price
+price = api.get_quotes(stock_code=stock,
+                exchange_code="NFO",
+                product_type="options",
+                expiry_date=expiry,
+                right=right,
+                strike_price=strike)
+
+# Get the buy price of order
+cost = float(api.get_order_detail('nfo',order_id)['Success'][0]['average_price'])
+trailing_stoploss = round(cost * 0.95,1)
+
+# Run Loop
+while(True):
+
+    if(price > cost * 1.10):
+        cost = price
+        trailing_stoploss = round(cost * 0.95,1)
+
+    elif(price < cost):
+
+        # Place square off order 
+        sq_off_order = api.square_off(exchange_code="NFO",
+                    product="options",
+                    stock_code=stock,
                     expiry_date=expiry,
                     right=right,
-                    strike_price=strike)
-    
-    # Get the execution price    
-    cost = float(api.get_order_detail('nfo',order_id)['Success'][0]['average_price'])
-    trailing_stoploss = round(cost * 0.95,1)
-
-    while(True):
-        
-        if(price > cost * 1.10):
-            cost = price
-            trailing_stoploss = round(cost * 0.95,1)
-
-        elif(price < cost):
-
-            # Place square off order 
-            sq_off_order = api.square_off(exchange_code="NFO",
-                        product="options",
-                        stock_code=stock,
-                        expiry_date=expiry,
-                        right=right,
-                        strike_price=strike,
-                        action='sell',
-                        order_type="market",
-                        validity="day",
-                        stoploss="",
-                        quantity="50",
-                        price="0",
-                        validity_date=datetime.now().strftime('%Y-%m-%dT06:00:00.000Z')  ,
-                        trade_password="",
-                        disclosed_quantity="0")
+                    strike_price=strike,
+                    action='sell',
+                    order_type="market",
+                    validity="day",
+                    stoploss="",
+                    quantity="50",
+                    price="0",
+                    validity_date=datetime.now().strftime('%Y-%m-%dT06:00:00.000Z')  ,
+                    trade_password="",
+                    disclosed_quantity="0")
 
 
-            if(sq_off_order['Status']==200) : 
-                print(sq_off_order)
+        if(sq_off_order['Status']==200) : 
+            print(sq_off_order)
 
-    
-            break
-        
-        time.sleep(2)
+
+        break
+
+    time.sleep(2)
