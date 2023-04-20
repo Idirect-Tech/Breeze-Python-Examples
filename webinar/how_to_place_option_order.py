b3217f91a06894cbca1d6167b282a189ac906c15
Idirect-Tech <115805427+Idirect-Tech@users.noreply.github.com>
2023-04-20 22:19:36 +0530

    Update how_to_place_option_order.py

--- webinar/how_to_place_option_order.py
+++ webinar/how_to_place_option_order.py
@@ -4,6 +4,17 @@ api_key = "INSERT_YOUR_APP_KEY_HERE"
 api_secret = "INSERT_YOUR_SECRET_KEY_HERE"
 api_session = 'INSERT_YOUR_API_SESSION_HERE'
 
+
+
+# Define Contract
+stock = 'NIFTY',
+strike = '17750',
+expiry = '2023-04-20T06:00:00.000Z',      
+right = 'call',         
+
+
+
+
 # Import Libraries
 
 from datetime import datetime
@@ -13,45 +24,20 @@ from breeze_connect import BreezeConnect
 api = BreezeConnect(api_key=api_key)
 api.generate_session(api_secret=api_secret,session_token=api_session)
 
+# Place order
+buy_order = api.place_order(stock_code=stock,
+                            exchange_code="NFO",
+                            product="options",
+                            action='buy',
+                            order_type='market',
+                            stoploss="",
+                            quantity="50",
+                            price="",
+                            validity="day",
+                            validity_date=today,
+                            disclosed_quantity="0",
+                            expiry_date=expiry,
+                            right=right,
+                            strike_price=strike)
 
-# Function to place order
-def place_order(each_leg):
-
-    today = datetime.now().strftime('%Y-%m-%dT06:00:00.000Z')    
-  
-    buy_order = api.place_order(stock_code=each_leg['stock'],
-                                exchange_code="NFO",
-                                product="options",
-                                action=each_leg['action'],
-                                order_type='market',
-                                stoploss='',
-                                quantity="50",
-                                price="",
-                                validity="day",
-                                validity_date=today,
-                                disclosed_quantity="0",
-                                expiry_date=each_leg['expiry'],
-                                right=each_leg['right'],
-                                strike_price=each_leg['strike'])
-
-    if(buy_order['Status']==200) : 
-        order_id = buy_order['Success']['order_id']
-        print(f'Successfully placed market order !\nOrder ID is {order_id}')
-        
-    else : 
-        print('\nFailed to place order!\n', buy_order['Error'])
-
-
-    
-if __name__ == "__main__":
-    print ("Starting Execution \n")
-        
-    # enter contract details
-    cx = {'stock': 'NIFTY',
-         'strike': '17750',
-         'expiry': '2023-04-20T06:00:00.000Z',      
-         'right': 'call',         
-         'action': 'buy'}
-    
-    # Place order
-    place_order(cx)
+print(buy_order)
