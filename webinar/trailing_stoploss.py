309a36bd4ef8681deb1fb612b999cd3bfe8f40b7
Idirect-Tech <115805427+Idirect-Tech@users.noreply.github.com>
2023-04-20 22:17:08 +0530

    Update trailing_stoploss.py

--- webinar/trailing_stoploss.py
+++ webinar/trailing_stoploss.py
@@ -13,19 +13,21 @@ from breeze_connect import BreezeConnect
 api = BreezeConnect(api_key=api_key)
 api.generate_session(api_secret=api_secret,session_token=api_session)
 
+if __name__ == "__main__":
+    print ("Starting Execution \n")
+        
+    # Define Contract
+    stock = 'NIFTY',
+    strike = '17750',
+    expiry = '2023-04-20T06:00:00.000Z',      
+    right = 'call',         
 
-# **************************************************************************************************************        
-
-# Function to generate a signal
-def place_order(each_leg):
-
-    today = datetime.now().strftime('%Y-%m-%dT06:00:00.000Z')    
-
-    # Place options order 
-    buy_order = api.place_order(stock_code=each_leg['stock'],
+    
+    # Place order
+    buy_order = api.place_order(stock_code=stock,
                                 exchange_code="NFO",
                                 product="options",
-                                action=each_leg['action'],
+                                action='buy',
                                 order_type='market',
                                 stoploss="",
                                 quantity="50",
@@ -33,106 +35,54 @@ def place_order(each_leg):
                                 validity="day",
                                 validity_date=today,
                                 disclosed_quantity="0",
-                                expiry_date=each_leg['expiry'],
-                                right=each_leg['right'],
-                                strike_price=each_leg['strike'])
+                                expiry_date=expiry,
+                                right=right,
+                                strike_price=strike)
 
-    if(buy_order['Status']==200) : 
-        order_id = buy_order['Success']['order_id']
-        print(f'Successfully placed market order !\nOrder ID is {order_id}')
-
-    else : 
-        print('\nFailed to place order!\n', buy_order['Error'])
-
-# **************************************************************************************************************        
-
-def square_off_at_market(each_leg):
-    
-    today = datetime.now().strftime('%Y-%m-%dT06:00:00.000Z')    
-    # Place square off order 
-    sq_off_order = api.square_off(exchange_code="NFO",
-                product="options",
-                stock_code=each_leg['stock'],
-                expiry_date=each_leg['expiry'],
-                right=each_leg['right'],
-                strike_price=each_leg['strike'],
-                action='sell',
-                order_type="market",
-                validity="day",
-                stoploss="",
-                quantity="50",
-                price="0",
-                validity_date=today,
-                trade_password="",
-                disclosed_quantity="0")
-
-
-    if(sq_off_order['Status']==200) : 
-        print(sq_off_order)
-
-    else : 
-        print('\nFailed to square off!\n', sq_off_order['Error'])
-        return False
-
-# ******************************************************************************************************************************            
-
-# Callback to receive ticks.
-# Event based function
-
-def on_ticks(ticks):    
-
-    ltp = ticks['last']        
-    print(f"LTP : {round(ltp,1)} | Level : {round(level,1)} | , SL : {round(trailing_stoploss,1)}")
-
-    # increase cost and SL if price level crosses threshold
-    if(ltp > level*1.05):      
-        print(">>>>  revising SL  <<<<<")
-        level = ltp
-        trailing_stoploss = level*0.95
-
-    # Square off if price level falls below trailing SL
-    if(ltp < trailing_stoploss):
-        #square off now
-        print(">>>>  ALERT : square off now  <<<<<")
-        square_off_at_market(cx)
-
-
-if __name__ == "__main__":
-    print ("Starting Execution \n")
-    
-    # Start streaming service i.e Websockets
-    api.ws_connect()
-    api.on_ticks = on_ticks
-    
-    # Select Contract
-    cx = {'stock': 'NIFTY',
-         'strike': '17750',
-         'expiry': '2023-04-20T06:00:00.000Z',      
-         'right': 'call',         
-         'action': 'buy'}
-    
-    # Place order
-    place_order(cx)
-
-    #Initialize variables 
-    cost, level, take_profit, trailing_stoploss = 0,0,0,0
-    
-    # Wait for few seconds for order to get successfully executed    
-    time.sleep(2)
+     
+    order_id = buy_order['Success']['order_id']
+        
+    price = api.get_quotes(stock_code=stock,
+                    exchange_code="NFO",
+                    product_type="options",
+                    expiry_date=expiry,
+                    right=right,
+                    strike_price=strike)
     
     # Get the execution price    
-    detail = api.get_order_detail('nfo',order_id)
-    cost = float(detail['Success'][0]['average_price'])
+    cost = float(api.get_order_detail('nfo',order_id)['Success'][0]['average_price'])
     trailing_stoploss = round(cost * 0.95,1)
 
-    print(f"Entry Cost : {cost}")
-    print(f"Setting SL : {trailing_stoploss}")
+    while(True):
+        
+        if(price > cost * 1.10):
+            cost = price
+            trailing_stoploss = round(cost * 0.95,1)
+
+        elif(price < cost):
+
+            # Place square off order 
+            sq_off_order = api.square_off(exchange_code="NFO",
+                        product="options",
+                        stock_code=stock,
+                        expiry_date=expiry,
+                        right=right,
+                        strike_price=strike,
+                        action='sell',
+                        order_type="market",
+                        validity="day",
+                        stoploss="",
+                        quantity="50",
+                        price="0",
+                        validity_date=datetime.now().strftime('%Y-%m-%dT06:00:00.000Z')  ,
+                        trade_password="",
+                        disclosed_quantity="0")
+
+
+            if(sq_off_order['Status']==200) : 
+                print(sq_off_order)
+
     
-    #subscribe to feeds
-    api.subscribe_feeds(exchange_code="NFO", 
-                    stock_code='NIFTY', 
-                    product_type="options", 
-                    expiry_date='20-Apr-2023', 
-                    strike_price='17750', 
-                    right='call', 
-                    get_exchange_quotes=True, get_market_depth=False)
+            break
+        
+        time.sleep(2)
